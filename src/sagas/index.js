fb8fd164939a105e5e9141b58b44939656a6d15e
padrisimo <adrianfernandezchamizo@gmail.com>
2019-11-16 16:04:24 +0100

    set-up redux-saga

---
+++ src/sagas/index.js
@@ -0,0 +1,5 @@
+function* rootSaga(params) {
+  console.log('root Saga here');
+}
+
+export default rootSaga;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
 const configureStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      applyMiddleware(sagaMiddleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
+  sagaMiddleware.run(rootSaga);
   return store;
 };
 
