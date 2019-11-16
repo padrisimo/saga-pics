554aeac83e577baa1193a96f6539da7dba12670a
padrisimo <adrianfernandezchamizo@gmail.com>
2019-11-16 21:35:51 +0100

    structuring sagas

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { loadImages } from '../../actions';
+
 import './styles.css';
 
 const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
@@ -33,6 +35,7 @@ class ImageGrid extends Component {
               <img src={image.urls.small} alt={image.user.username} />
             </div>
           ))}
+          <a onClick={this.props.loadImages}>Load Images</a>
         </section>
       </div>
     );
@@ -45,7 +48,11 @@ const mapStateToProps = ({ isLoading, images, error }) => ({
   error
 });
 
+const mapDispatchToProps = dispatch => ({
+  loadImages: () => dispatch(loadImages())
+});
+
 export default connect(
   mapStateToProps,
-  null
+  mapDispatchToProps
 )(ImageGrid);
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,16 @@
-function* rootSaga(params) {
-  console.log('root Saga here');
+import { takeEvery } from 'redux-saga/effects';
+import { IMAGES } from '../constants';
+
+// worker saga
+function* handleImagesLoad() {
+  console.log('fetch pics from api');
 }
+
+// watcher saga
+function* rootSaga() {
+  yield takeEvery(IMAGES.LOAD, handleImagesLoad);
+}
+
+// watcher saga -> actions -> worker saga
 
 export default rootSaga;
