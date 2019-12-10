fbc77457934491f6c3f6232c5e00aa20b48150e4
Adrian Fernandez <chamizho@gmail.com>
2019-12-10 15:53:27 +0100

    hotfix compose reduxdevtools

--- src/store/index.js
+++ src/store/index.js
@@ -4,15 +4,14 @@ import createSagaMiddleware from 'redux-saga';
 import rootSaga from '../sagas';
 
 const configureStore = () => {
+  const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+      : compose;
+
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+  const store = createStore(rootReducer, enhancer);
   sagaMiddleware.run(rootSaga);
   return store;
 };
